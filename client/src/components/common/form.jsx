fce64d049c186f391c880f1c9ea1b7dc6d4cc42f
yared-abera <yaredoabera7@gmail.com>
2024-11-26 14:18:04 +0300

    modified common form component

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -2,12 +2,54 @@
 
 
 import React from 'react'
+import { Input } from '../ui/input';
 
  function CommonForm({formControls}) {
+  
+
+   function renderInputByComponentType(getControlItem)
+   {
+     let element=null;
+     
+     switch(getControlItem.componentType){
+      case "input":
+         element= (
+            <Input
+             name={getControlItem.name}
+             placeholder={getControlItem.placeholder}
+             id={getControlItem.name}
+             type={getControlItem.type}
+             />   
+          );
+          break;
+         default :
+         element= (
+            <Input
+             name={getControlItem.name}
+             placeholder={getControlItem.placeholder}
+             id={getControlItem.name}
+             type={getControlItem.type}
+             />);
+
+     }
+
+   }
+
+
+
+
+
+
+
   return (
      <form>
       <div className='flex flex-col gap-3'>
-         
+         { formControls.map(controlItem=> <div className='grid w-full gap-1.5'>
+            <Label className="mb-1>">{controlItem.label}</Label>
+            {
+               renderInputByComponentType(controlItem)
+            }
+         </div>)}
         </div>  
      </form>
   )
