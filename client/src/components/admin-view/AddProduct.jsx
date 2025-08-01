6604cb84aae55c4bd29605e41b2084984eb539ef
yared-abera <yaredoabera7@gmail.com>
2025-08-01 22:57:55 +0300

    modified

--- client/src/components/admin-view/AddProduct.jsx
+++ client/src/components/admin-view/AddProduct.jsx
@@ -2,9 +2,11 @@
 import React from 'react'
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '../ui/sheet'
 
-const AddProduct = () => {
+const AddProduct = ({open,setOpen}) => {
   return (
-      <Sheet open={} onOpenChange={ () => {}} >
+      <Sheet open={open} onOpenChange={ () => {
+        setOpen=(false)
+      }} >
         <SheetContent>
           <SheetHeader>
             <SheetTitle>Add New Product</SheetTitle>
--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -2,13 +2,19 @@
 import React from 'react';
 import { Button } from '../../components/ui/button';
 import AddProduct from '@/components/admin-view/AddProduct';
-
+import 
 function AdminProducts() {
-    return (
 
-         
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] = useState(false);
+
+
+    return ( 
+
+        
          <div className="flex flex-col gap-4 p-6 bg-white rounded-lg shadow-lg">
-             <AddProduct />
+
+             <AddProduct open={openCreateProductsDialog} setOpen={setOpenCreateProductsDialog} /> 
+
               <h1 className="text-2xl font-bold mb-4">Admin Products Page</h1>  
               <div className="flex justify-end gap-2 ">
                  <Button className="bg-yellow-600 p-2 mx-6 hover:bg-yellow-500 text-black text-md">Add Laptop</Button>
